a2c652adb2bd2e7965438251aa15f608d580f860
sonicyang <sonic.tw.tp@gmail.com>
2016-04-27 04:26:03 -0700

    do print out error message according to errno, if errno is 0

--- inc/misc.h
+++ inc/misc.h
@@ -2,9 +2,13 @@
 #define __MISC_H__
 
 #include <stdio.h>
+#include <errno.h>
 
 extern inline void die(const char *msg){
-    perror(msg);
+    if(!errno)
+        fprintf(stderr, "%s\n", msg);
+    else
+        perror(msg);
     exit(1);
 }
 
