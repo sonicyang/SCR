0a05402da174cb678176587cf31a4a41f1d42118
sonicyang <sonic.tw.tp@gmail.com>
2016-05-02 02:19:23 -0700

    Properly cleanup the resources

--- src/server/server.c
+++ src/server/server.c
@@ -11,63 +11,91 @@
 #include <stdint.h>
 #include <ctype.h>
 #include <pthread.h>
+#include<signal.h>
 
 #include "misc.h"
 #include "setting.h"
 
 #define OPTION "f:"
 
-struct client_argument_t{
+struct client_t{
     struct sockaddr_in address;
     socklen_t length;
     int socket;
     pthread_t thread_id;
 };
 
-void client_handler(struct client_argument_t* argument){
-     int sockfd, newsockfd, portno;
-     char buffer[256];
-     int n;
-     if (newsockfd < 0)
-          die("die on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) die("die reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) die("die writing to socket");
-     close(newsockfd);
-     close(sockfd);
-     return;
+struct listener_t{
+    struct sockaddr_in address;
+    int socket;
+    pthread_t thread_id;
+};
+
+void client_clean_up(void* argument){
+    struct client_t* tmp = (struct client_t*) argument;
+    close(tmp->socket);
 }
 
-int start_client_handler(struct client_argument_t* arg){
+void client_handler(struct client_t* argument){
+    char buffer[256];
+    int byte_read;
+
+    pthread_cleanup_push(client_clean_up, argument);
+
+    byte_read = read(argument->socket ,buffer, 255);
+
+    if (byte_read < 0)
+        die("die reading from socket");
+
+    printf("Here is the message: %s\n",buffer);
+
+    /*n = write(newsockfd,"I got your message",18);*/
+    /*if (n < 0) die("die writing to socket");*/
+
+    pthread_cleanup_pop(1);
+    return;
+}
+
+int start_client_handler(struct client_t* arg){
     if(pthread_create(&(arg->thread_id), NULL, (void*)client_handler, (void*)arg)){
         die("Failed on creating Client Handler thread");
     }
     return 0;
 }
 
-struct client_argument_t* wait_for_client(int* listener_sock){
-    struct client_argument_t* arg;
+struct client_t* wait_for_client(int* listener_sock){
+    struct client_t* arg;
 
-    arg = (struct client_argument_t*)malloc(sizeof(struct client_argument_t));
+    arg = (struct client_t*)malloc(sizeof(struct client_t));
     arg->length = sizeof(arg->address);
     arg->socket = accept(*listener_sock, (struct sockaddr*)&(arg->address), &(arg->length));
+
+    if(arg->socket < 0){
+        die("Failed to accept connection");
+    }
 
     start_client_handler(arg);
 
     return arg;
 }
 
+void listener_clean_up(void* argument){
+    int* sock = (int*)argument;
+    close(*sock);
+}
+
 void listener(struct setting_t* setting){
     int listener_sock;
     struct sockaddr_in address;
     int connected_user = 0;
-    struct client_argument_t** clients;
+    struct client_t** clients;
+    int i;
+
+    pthread_cleanup_push(listener_clean_up, &listener_sock);
 
     printf("LISTENER   |  Initialzing...\n");
-    clients = (struct client_argument_t**)malloc(setting->max_user * sizeof(struct client_argument_t*));
+    clients = (struct client_t**)malloc(setting->max_user * sizeof(struct client_t*));
+    memset(clients, 0, setting->max_user * sizeof(struct client_t*));
 
     printf("LISTENER   |  Opening Socket...\n");
     listener_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -86,18 +114,36 @@ void listener(struct setting_t* setting){
         die("Cannot listen on the socket");
 
     while(connected_user < setting->max_user){
-        clients[connected_user++] = wait_for_client(&listener_sock);
+        for(i = 0; i < setting->max_user; i++){
+            if(clients[i] == 0){
+                clients[i] = wait_for_client(&listener_sock);
+                break;
+            }
+        }
     }
 
-    close(listener_sock);
+    for(i = 0; i < setting->max_user; i++){
+        if(clients[i] != 0)
+            pthread_cancel(clients[i]->thread_id);
+    }
+
+    pthread_cleanup_pop(1);
     return;
 }
 
-int start_listener(struct setting_t setting, pthread_t* listener_id){
-    if(pthread_create(listener_id, NULL, (void*)listener, (void*)&setting)){
+int start_listener(struct setting_t* setting, pthread_t* listener_id){
+    if(pthread_create(listener_id, NULL, (void*)listener, (void*)setting)){
         die("Failed on creating Listener thread");
     }
     return 0;
+}
+
+struct listener_t listener_instance;
+
+void signal_handler(int signal_number)
+{
+    if (signal_number == SIGINT)
+        pthread_cancel(listener_instance.thread_id);
 }
 
 int main(int argc, char *argv[]){
@@ -105,8 +151,8 @@ int main(int argc, char *argv[]){
     char setting_fn[64] = "\0";
     struct setting_t setting;
 
-    pthread_t  listener_id;
-
+    if (signal(SIGINT, signal_handler) == SIG_ERR)
+        die("Cannot handle SIGINT");
 
     while(1){
         cmd_opt = getopt(argc, argv, OPTION);
@@ -133,11 +179,11 @@ int main(int argc, char *argv[]){
     parse_setting(setting_fn, &setting);
     printf("Done\n");
 
-    printf("Creating Listener thread...");
-    start_listener(setting, &listener_id);
+    printf("Creating listener_instance thread...");
+    start_listener(&setting, &(listener_instance.thread_id));
     printf("Done\n");
 
-    pthread_join(listener_id, NULL);
+    pthread_join(listener_instance.thread_id, NULL);
 
     exit(0);
 
