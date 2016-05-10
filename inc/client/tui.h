59da10d177d2f61a2ec13bb469303bbb1a29f700
sonicyang <sonic.tw.tp@gmail.com>
2016-05-10 17:18:18 +0800

    use separate thread to print and use list to maintain message, make input handler a command handler

--- inc/client/tui.h
+++ inc/client/tui.h
@@ -2,6 +2,10 @@
 #define __TUI_H__
 
 #include <curses.h>
+#include <semaphore.h>
+
+#include "list.h"
+#include "message.h"
 
 struct TUI_t;
 
@@ -16,15 +20,25 @@ struct registered_command_t{
 struct TUI_t{
     int run;
     struct list_t* command_chain;
-    command_handler_t default_input_callback;
+    int line;
+    struct registered_command_t default_input_callback;
+    struct list_t* message_list;
+    struct list_element_t* print_start;
+    sem_t* print;
+    pthread_t print_thread_id;
     WINDOW* message_window;
     WINDOW* command_window;
 };
 
-void TUI_init(struct TUI_t*, command_handler_t);
+void TUI_init(struct TUI_t*);
 void TUI_process(struct TUI_t*);
+void TUI_refresher(struct TUI_t*);
 void TUI_stop(struct TUI_t*);
 void TUI_terminate(struct TUI_t*);
+void TUI_write_message(struct TUI_t*, struct message_t*);
+void TUI_error(struct TUI_t*, char*);
+void TUI_print(struct TUI_t*, char*);
+void clear_win(WINDOW*);
 
 void TUI_register_command(struct TUI_t*, char*, command_handler_t, void*);
 
--- src/client/tui.c
+++ src/client/tui.c
@@ -1,5 +1,7 @@
 #include <curses.h>
 #include <string.h>
+#include <errno.h>
+#include <semaphore.h>
 
 #include "tui.h"
 #include "list.h"
@@ -7,13 +9,16 @@
 
 static WINDOW *create_newwin(int, int, int, int);
 static void destroy_win(WINDOW*);
-static void clear_win(WINDOW*);
 
 static void TUI_prompt_welcome(struct TUI_t*);
 
-void TUI_init(struct TUI_t* tui, command_handler_t handler){
+void TUI_init(struct TUI_t* tui){
     tui->command_chain = create_list(sizeof(struct registered_command_t));
-    tui->default_input_callback = handler;
+    tui->default_input_callback.handler = NULL;
+    tui->line = 1;
+
+    sem_unlink("PRINT_SEM");
+    tui->print = sem_open("PRINT_SEM", O_CREAT, O_RDWR, 0);
 
     initscr();
     cbreak();
@@ -22,6 +27,15 @@ void TUI_init(struct TUI_t* tui, command_handler_t handler){
     tui->command_window = create_newwin(3, COLS, LINES - 3, 0);
 
     keypad(tui->command_window, TRUE);
+
+    tui->message_list = create_list(sizeof(struct message_t));
+    //Dummy Head
+    tui->print_start = list_allocate(tui->message_list);
+    init_message(tui->print_start->data, "", 0);
+
+    if(pthread_create(&(tui->print_thread_id), NULL, (void*)TUI_refresher, (void*)tui)){
+         die("Error on creating Refresher");
+    }
 
     TUI_prompt_welcome(tui);
 }
@@ -43,32 +57,84 @@ void TUI_process(struct TUI_t* tui){
                 tmp = ((struct registered_command_t*)ptr->data);
 
                 if(!strcmp(tmp->command, input + 1)){
-                    (*(tmp->handler))(tui, tmp->argument);
+                    (*(tmp->handler))(tui, input + strlen(tmp->command) + 2, tmp->argument);
                 }
                 ptr = ptr->next;
             }
         }else{
-            if(tui->default_input_callback)
-                 (*(tui->default_input_callback))(tui, input);
+            if(tui->default_input_callback.handler)
+                 (*(tui->default_input_callback.handler))(tui, input, tui->default_input_callback.argument);
         }
 
         clear_win(tui->command_window);
-        wrefresh(tui->message_window);
         wrefresh(tui->command_window);
     }
 }
 
+void TUI_refresher(struct TUI_t* tui){
+    struct list_element_t* ptr;
+    int i;
+
+    while(tui->run){
+        sem_wait(tui->print);
+        clear_win(tui->message_window);
+
+        i = 1;
+        ptr = tui->print_start;
+        while(ptr != NULL){
+            mvwprintw(tui->message_window, i, 1, ((struct message_t*)ptr->data)->buffer);
+            i++;
+            ptr = ptr->next;
+        }
+
+        wrefresh(tui->message_window);
+    }
+}
+
+void TUI_write_message(struct TUI_t* tui, struct message_t* message_in){
+    struct list_element_t* message = list_allocate(tui->message_list);
+    memcpy(message->data, message_in, sizeof(struct message_t));
+
+    if(tui->message_list->size > LINES - 3 - 2)
+        tui->print_start = tui->print_start->next;
+
+    sem_post(tui->print);
+}
+
+void TUI_error(struct TUI_t* tui, char* str){
+    struct message_t message;
+
+    init_message(&message, "ERROR", strlen(str));
+    strcpy(message.buffer, str);
+    TUI_write_message(tui, &message);
+    if(errno){
+        init_message(&message, "ERROR", strlen(strerror(errno)));
+        strcpy(message.buffer, strerror(errno));
+        TUI_write_message(tui, &message);
+    }
+
+    return;
+}
+
 void TUI_stop(struct TUI_t* tui){
      tui->run = 0;
+     pthread_join(tui->print_thread_id, NULL);
      return;
 }
 
 void TUI_terminate(struct TUI_t* tui){
+    sem_unlink("PRINT_SEM");
+    delete_list(tui->message_list);
     endwin();
 }
 
 void TUI_register_command(struct TUI_t* tui, char* command, command_handler_t handler, void* argument){
-    struct registered_command_t* tmp = list_allocate(tui->command_chain)->data;
+    struct registered_command_t* tmp;
+    if(strlen(command)){
+        tmp = list_allocate(tui->command_chain)->data;
+    }else{
+        tmp = &tui->default_input_callback;
+    }
     strncpy(tmp->command, command, 63);
     tmp->command[63] = '\0';
 
