f425a0160e70e8c21ce782efe7ecb42701a48251
sonicyang <sonic.tw.tp@gmail.com>
2016-05-12 00:52:05 +0800

    Enable the message window to scroll

--- src/client/tui.c
+++ src/client/tui.c
@@ -20,7 +20,7 @@ void clear_win(WINDOW* local_win);
 void TUI_init(struct TUI_t* tui){
     tui->command_chain = create_list(sizeof(struct registered_command_t));
     tui->default_input_callback.handler = NULL;
-    tui->line = 1;
+    tui->line = 0;
 
     sem_unlink("APRINT_SEM");
     tui->print = sem_open("APRINT_SEM", O_CREAT|O_EXCL, S_IRWXU, 0);
@@ -159,17 +159,25 @@ static void TUI_prompt_welcome(struct TUI_t* tui){
 
 static void TUI_refresh(struct TUI_t* tui){
     struct list_element_t* ptr;
-    int i;
+    int i, j;
     char now[32];
     struct tm tim;
 
-    clear_win(tui->message_window);
-
+    ptr = tui->message_list->tail;
+    for(i = 1; i < LINES - 3 - 2; i++){
+        if(ptr->prev != NULL){
+             ptr = ptr->prev;
+        }else{
+            break;
+        }
+    }
     i = 1;
-    ptr = tui->print_start;
     while(ptr != NULL){
         localtime_r((time_t*)&((struct message_t*)ptr->data)->timestamp, &tim);
         strftime(now, 30, "%X", &tim);
+        wmove(tui->message_window, i, 1);
+        for(j = 0; j < COLS - 2; j++)
+            wprintw(tui->message_window, " ");
         mvwprintw(tui->message_window, i, 1, now);
         mvwprintw(tui->message_window, i, 12, ((struct message_t*)ptr->data)->sender);
         mvwprintw(tui->message_window, i, 30, ((struct message_t*)ptr->data)->buffer);
